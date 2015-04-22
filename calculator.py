1778b850e17f1dc7c7dacce2211ed23bd6719aca
mwas <francismwangi152@gmail.com>
2015-04-22 08:45:39 +0300

    added events for few buttons

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 class Calculator(Frame):
     def __init__(self):
+        self.expression = []
         Frame.__init__(self)
         self.master.title("Calculator")
         
@@ -8,14 +9,20 @@ class Calculator(Frame):
         self.master.rowconfigure(0, weight =1)
         self.master.columnconfigure(0, weight =1)
         self.grid(sticky=W+E+N+S)
+
                                     
         self.entry=Entry(self)
         self.entry.grid(row=0, columnspan=4, sticky=W+E+N+S)
+        self.entry.insert(INSERT, "enter text here")
         
         self.button1=Button(self, text="7")
         self.button1.grid(row=1, column=0, sticky=W+E+N+S)
+        self.button1.bind('<Button-1>', lambda event: self.command(7, event))
+
         self.button2=Button(self, text="8")
         self.button2.grid(row=1, column=1, sticky=W+E+N+S)
+        self.button2.bind('<Button-1>', lambda event: self.command(8, event))
+
         self.button3=Button(self, text="9")
         self.button3.grid(row=1, column=2, sticky=W+E+N+S)
         self.button4=Button(self, text="/")
@@ -47,6 +54,26 @@ class Calculator(Frame):
         self.button15.grid(row=4, column=2, sticky=W+E+N+S)
         self.button16=Button(self, text="+")
         self.button16.grid(row=4, column=3, sticky=W+E+N+S)
+
+    def command(self, button_selected, event):
+        """
+        it adds the button selected to expression
+        it also replace the digit in entry with one selected
+        prints which button has been pressed for debugging purpose
+        :param button_selected: string to the button selected
+        :return: None
+        """
+
+        # clear the entry first
+        self.entry.delete(0, len(self.entry.get()))
+        # then add the selected button
+        self.entry.insert(INSERT, button_selected)
+        self.expression.append(button_selected)
+        print "button_selected:", button_selected
+
+
+
+
 def main():
     Calculator().mainloop()
 if __name__ == "__main__":
